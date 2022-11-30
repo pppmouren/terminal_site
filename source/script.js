d7b6bee889ded51eebdd4e6ceecefb15be29b302
handsomedad <handsomedad@Xuhong-LindeMacBook-Pro.local>
2022-11-29 21:30:12 -0500

    terminal site

--- source/script.js
+++ source/script.js
@@ -1,18 +1,31 @@
 var commands = document.getElementById("commands");
 var user_input = document.getElementById("user_input");
-var terminal_output = document.getElementById("terminal_output");
+var terminal_outputs = document.getElementById("terminal_outputs");
+var terminal_content = document.getElementById("terminal_content");
 
 function execute(input){
     let output;
     input = input.toLowerCase();
-    output = `<div>→ testing the input is ${input}</div>`;
-    terminal_output.innerHTML = `${terminal_output.innerHTML+output}`;
+    output = `<div>→ ${input}</div>`;
+    if(!COMMANDS.hasOwnProperty(input)){
+        output += `<div> <span class="command"> No such command:</span> ${input} </div>`;
+    }
+    else{
+        output += `<div>${COMMANDS[input]} </div>`;
+    }
+    
+    terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
+    terminal_content.scrollTop = terminal_content.scrollHeight;
+
+    if(input == "github"){
+        setTimeout(() => {window.open("https://github.com/pppmouren", "_blank");}, 2000);
+    }
 }
 
 function key(e){
     const input = user_input.value;
 
-    if (e.key == "Enter"){
+    if(e.key == "Enter"){
         execute(input);
         user_input.value = "";
         return;
@@ -22,3 +35,22 @@ function key(e){
 }
 
 document.addEventListener("keypress", key);
+
+const COMMANDS = 
+{
+    help:
+        'Supported commands: <span class="command-keyword">"about"</span>, <span class="command-keyword">"experience"</span>, <span class="command-keyword">"education"</span>, <span class="command-keyword">"skills"</span>, <span class="command-keyword">"contact"</span>, <span class="command-keyword">"github"</span>',
+    about:
+        "Hello, my name is pppmouren",
+    experience:
+        'I have college life',
+    education:
+        'I am in clooege',
+    skills:
+        'I can get up at 8am without setting clock',
+    contact:
+        'You can never find me! HAHAHA!',
+    github:
+        'opening my github page ...',
+    
+}
